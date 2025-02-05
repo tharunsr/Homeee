fe0c86d77279238ee7a1a14efa1fc2a498edc888
tharunsr <tharunsrinivaassr@gmail.com>
2025-02-05 23:39:16 +0530

    Home is above

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './HomePage.css';
 import Hero from './Hero';
 import logo from '../assets/beautybasket.png'
+import Cognizant from '../assets/Cognizant-Logo.png'
+import Garnier from '../assets/Garnier-logo.png'
+import Loreal from '../assets/LOreal-Logo.png'
+import Beardo from '../assets/Beardo.png'
 
 const HomePage = () => {
   return (
@@ -24,9 +28,18 @@ const HomePage = () => {
       </header>
       <main className="main-content">
       <Hero />
+      <div className="marquee-container">
+      <div className='marquee-two'>
+      <img src={Cognizant} alt="Image 1" />
+      <img src={Garnier} alt="Image 2" />
+      <img src={Loreal}alt="Image 3" />
+      <img src={Beardo} alt="Image 1" />
+      </div>
+      </div>
       </main>
+      <div>sd</div>
       <footer className="footer">
-        <p>Footer content here.</p>
+        <p>Copyright BeautyBasket</p>
       </footer>
     </div>
   );
